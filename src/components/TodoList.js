6b3ad79342c7c8866ccf3aa27b53b9cb82780d27
고은 <koeun0529@gmail.com>
2021-03-11 17:49:39 +0900

    [fix] TodoEditor 분기 처리 한줄로 수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,60 +1,64 @@
 import React, { useState } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-import Header from "./Header";
-import TodoItem from "./TodoItem";
+import Header from './Header';
+import TodoItem from './TodoItem';
 import RemainTodoList from './RemainTodoList';
 import TodoEditor from './TodoEditor';
 
-import {togglePage} from "../modules/PageToggleReducer";
+import { togglePage } from '../modules/PageToggleReducer';
 
 const ToggleButton = styled.button`
-font-size:32px;
-width:32px;
-height:32px;
-display:flex;
-justify-content:center;
-align-items:center;
-margin:0 auto;
-margin-top:70px;
+  font-size: 32px;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 70px;
 `;
 
 const Wrapper = styled.div`
-width:100%;
-height:100%;
-display:flex;
-flex-direction:column;
-justify-content:space-between;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-
 export default function TodoList() {
-    const [editButtonSelected,setEditButtonSelected]=useState(false);
-    const [selectedTodo,setSelectedTodo]=useState();
+  const [editButtonSelected, setEditButtonSelected] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState();
 
-    const dispatch=useDispatch();
-    const todos = useSelector(state => state.TodosReducer);
-    const toggleButtonSelected=useSelector(state=>state.PageToggleReducer);
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.TodosReducer);
+  const toggleButtonSelected = useSelector((state) => state.PageToggleReducer);
 
-    const handlePageToggle=()=>dispatch(togglePage());
+  const handlePageToggle = () => dispatch(togglePage());
 
-    return (
-            toggleButtonSelected 
-            ?
-                editButtonSelected 
-                ? <TodoEditor text="수정하기" title="Edit Todo" selectedTodo={selectedTodo} setEditButtonSelected={setEditButtonSelected} />
-                : <TodoEditor text="추가하기" title="Add Todo" selectedTodo={selectedTodo} setEditButtonSelected={setEditButtonSelected} />    
-            :
-                <Wrapper>
-                    <Header text='투-두 리스트' />
-                    <RemainTodoList todos={todos} /> 
-                    {todos.map(todo => (
-                        <TodoItem key={todo.id} todo={todo} editButtonSelected={editButtonSelected} setEditButtonSelected={setEditButtonSelected} setSelectedTodo={setSelectedTodo}/>
-                    ))}
-                    <ToggleButton onClick={handlePageToggle}>+</ToggleButton>
-                </Wrapper>
-    );
+  return toggleButtonSelected ? (
+    <TodoEditor
+      text={editButtonSelected ? '수정하기' : '추가하기'}
+      title={editButtonSelected ? 'Edit Todo' : 'Add Todo'}
+      selectedTodo={selectedTodo}
+      setEditButtonSelected={setEditButtonSelected}
+    />
+  ) : (
+    <Wrapper>
+      <Header text="투-두 리스트" />
+      <RemainTodoList todos={todos} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          editButtonSelected={editButtonSelected}
+          setEditButtonSelected={setEditButtonSelected}
+          setSelectedTodo={setSelectedTodo}
+        />
+      ))}
+      <ToggleButton onClick={handlePageToggle}>+</ToggleButton>
+    </Wrapper>
+  );
 }
-
-
