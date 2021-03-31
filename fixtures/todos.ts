30f9169478e60313cf880d7ce67e0f83d081e17f
고은 <koeun0529@gmail.com>
2021-03-31 16:38:27 +0900

    [feat] 테스팅에 필요한 데이터들 fixtures 폴더에 추가

---
+++ fixtures/todos.ts
@@ -0,0 +1,16 @@
+const todos = [
+  {
+    id: 1,
+    todoContent: 'todoContent 예시1 : 빨강',
+    todoColor: '#ffafb0',
+    done: false,
+  },
+  {
+    id: 2,
+    todoContent: 'todoContent 예시2 : 주황',
+    todoColor: '#ffc282',
+    done: false,
+  },
+];
+
+export default todos;
